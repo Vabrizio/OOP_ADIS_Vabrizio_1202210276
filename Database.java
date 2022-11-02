2d7a30249187527aab47de7ed84b1a2da78e4eb6
Vabrizio <116418240+Vabrizio@users.noreply.github.com>
2022-11-02 15:43:43 +0700

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+list<menu> listmenu= new Arraylist<mrnu>();
+    // TODO Create Method to Insert Menu to Database
+public void insertmenu(menu menu){
+
+}
+    // TODO Create Method to Show Menu from Database
+public void showmenu(menu menu){
+
+}
+    // TODO Create Method to Search Menu from Database
+public void searchmenu(menu menu){
+    
+}
+
+}
